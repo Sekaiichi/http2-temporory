c1868cccdba84f98fcfeeda71fed5628e1796d5d
Mehrubon Khusaynov <husaynov888@gmail.com>
2020-11-17 09:53:22 +0500

    first commit

---
+++ cmd/main.go
@@ -0,0 +1,36 @@
+package main
+
+import (
+	"net"
+	"net/http"
+	"os"
+
+	"github.com/sekaiichi/http2-temporory/cmd/app"
+	"github.com/sekaiichi/http2-temporory/pkg/banners"
+)
+
+func main() {
+	//обьявляем порт и хост
+	host := "0.0.0.0"
+	port := "9999"
+
+	if err := execute(host, port); err != nil {
+		os.Exit(1)
+	}
+}
+
+func execute(h, p string) error {
+	mux := http.NewServeMux()
+
+	bannerSvc := banners.NewService()
+
+	server := app.NewServer(mux, bannerSvc)
+
+	server.Init()
+
+	srv := &http.Server{
+		Addr:    net.JoinHostPort(h, p),
+		Handler: server,
+	}
+	return srv.ListenAndServe()
+}
---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/sekaiichi/http2-temporory
+
+go 1.15
